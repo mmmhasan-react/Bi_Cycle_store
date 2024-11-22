ce402ba13a97f7cbe971063bc4b32c9db05b318a
mmmhasan-react <2md.mahmudul.hasan@gmail.com>
2024-11-22 11:48:18 +0600

    env

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ async function main() {
     await mongoose.connect(config.dl_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`Exam ple app listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
